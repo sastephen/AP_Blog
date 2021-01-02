b3fcce49aaeb4e8e225893cad874898754787918
Stephen <hello@stephen.vip>
2021-01-02 14:51:24 +0630

    week-3 assignment

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>AP Laravel</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<h2 style="text-align:center">Sample Blog Site</h2>
+<body>
+    @yield('content')
+</body>
+</html>
